07edb64938a990ca511f0029b9327e6a3882ea4b
Z-XS <43204852+Z-XS@users.noreply.github.com>
2019-04-02 18:07:20 +0800

    Add files via upload

---
+++ node/app.js
@@ -0,0 +1,62 @@
+var express=require('express');
+var app =express();
+var mysql = require('mysql')
+var bodyParser = require('body-parser'); 
+//引用bodyParser 
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+//连接数据库
+var connection = mysql.createConnection({
+    host : 'localhost',
+    user : 'root',
+    password : '123456',
+    database : 'text'
+})
+connection.connect()
+var addSql = 'INSERT INTO supform(qd,name,offer) VALUES ?'
+//设置跨域访问
+app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Content-Type,XFILENAME,XFILECATEGORY,XFILESIZE");
+    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+    res.header("X-Powered-By",' 3.2.1');
+    res.header("Content-Type", "application/json;charset=utf-8");
+    next();
+ });
+ var questions=[
+    {
+    data:213,
+    num:444,
+    age:12
+    },
+    {
+    data:456,
+    num:678,
+    age:13
+    }];
+//写个接口123
+app.get('/123',function(req,res){
+    res.status(200),
+    res.json(questions)
+    });
+app.post('/w',function(req,res){
+    console.log(req.body);
+    var SqlParams = req.body
+    connection.query(addSql,[SqlParams],function(err,result){
+        if(err){
+            console.log(err.message + '失败')
+            return
+        }
+        console.log('加入',result)
+    })
+    // connection.end()
+    res.json(req.body)
+})
+//配置服务端口
+var server = app.listen(3001,'localhost',function () {
+
+    var host = server.address().address;
+     var port = server.address().port;
+
+        console.log("Example app listening at http://%s:%s", host, port);
+    })
