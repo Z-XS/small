b46961c4f08d9e5d90e05ad226991925f0661bd3
Z-XS <43204852+Z-XS@users.noreply.github.com>
2019-04-04 18:04:26 +0800

    Add files via upload

---
+++ 祝福语/datas/list_datas.js
@@ -0,0 +1,13 @@
+let list_data = [
+  {
+    holiday:'除夕',
+    content: [1,2.2,3]
+  },
+  {
+    holiday: '春节',
+    content: [2, 5, 6]
+  },
+  ]
+  module.exports = {
+    list_data
+  }
---
+++ 祝福语/pages/index/index.js
@@ -0,0 +1,79 @@
+// pages/index/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    ArrHoliday:[
+      {Dayname: '除夕', bgCol: '#F4D74B'},
+      {Dayname: '春节', bgCol: '#F395AD'},
+      {Dayname: '元旦', bgCol: '#85C062'},
+      {Dayname: '生日', bgCol: '#FF8C33'},
+      {Dayname: '国庆节', bgCol: '#9FCE4A'},
+      {Dayname: '端午节', bgCol: '#9F94BD'},
+      {Dayname: '清明节', bgCol: '#4B8EAB'},
+      {Dayname: '劳动节', bgCol: '#9F94BD'},
+      {Dayname: '开业', bgCol: '#F4D74B'},
+      {Dayname: '结婚', bgCol: '#F395AD'},
+      {Dayname: '生子', bgCol: '#85C062'},
+      {Dayname: '圣诞节', bgCol: '#FF8C33'}
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
